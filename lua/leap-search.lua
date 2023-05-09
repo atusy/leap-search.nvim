2b415f8c2dd65f065c241358511a97cee4cfa69f
atusy <30277794+atusy@users.noreply.github.com>
2023-05-10 00:11:27 +0900

    feat: highlight matches

--- lua/leap-search.lua
+++ lua/leap-search.lua
@@ -3,23 +3,32 @@
 ---@field ignorecase boolean?
 ---@field magic boolean?
 ---@field smartcase boolean?
+---@field hl_group? string
+local opts_default = {
+  engine = "vim.regex",
+  hl_group = "Search"
+}
 
 ---@param pat string
 ---@param opts matchopts?
 ---@return boolean
 local function leap(pat, opts)
-  local o = opts or {}
-  local engine = o.engine or "vim.regex"
+  local o = vim.tbl_deep_extend("keep", opts or {}, opts_default)
   -- search for leap targets
-  local targets = require("leap-search.engine." .. engine).search(pat, 0, o)
+  local targets = require("leap-search.engine." .. o.engine).search(pat, 0, o)
   if #targets == 0 then
     return false
   end
 
   -- prepare autocmd to be invoked on LeapEnter and removed subsequently
   local autocmd
+  local ns = vim.api.nvim_create_namespace("")
+  local bufs = {}
   local function del()
     pcall(vim.api.nvim_del_autocmd, autocmd)
+    for b, _ in pairs(bufs) do
+      vim.api.nvim_buf_clear_namespace(b, ns, 0, -1)
+    end
   end
 
   -- avoid hiding matched strings by placing labels before the matches
@@ -28,6 +37,12 @@ local function leap(pat, opts)
     once = true,
     callback = function()
       for _, t in pairs(require("leap").state.args.targets) do
+        local b = (t.wininfo and t.wininfo[1].bufnr or 0)
+        bufs[b] = true
+        vim.api.nvim_buf_set_extmark(t.wininfo and t.wininfo[1].bufnr or 0, ns, t.pos[1] - 1, t.pos[2] - 1, {
+          end_col = t.pos[3] - 1,
+          hl_group = o.hl_group
+        })
         local p = t.pos[2]
         if p > 1 then
           t.pos[2] = p - 1
