ff41299c1cf3290b6dbec6f8cb2226468d5a34ea
atusy <30277794+atusy@users.noreply.github.com>
2023-05-11 23:10:28 +0900

    feat: support specifying multiple engines and merge matches

--- lua/leap-search.lua
+++ lua/leap-search.lua
@@ -20,6 +20,48 @@ local function action(t)
   })
 end
 
+---@param opts_engine Opts_engine
+---@param opts_leap table
+---@return {pos: {[1]: integer, [2]: integer, [3]: integer}}[]
+local function _search(pat, opts_engine, opts_leap)
+  return require("leap-search.engine." .. opts_engine.name).search(pat, opts_engine, opts_leap)
+end
+
+---@param opts_match Opts_match
+---@param opts_leap table
+local function search(pat, opts_match, opts_leap)
+  -- if a single engine simply return matches
+  if #opts_match.engines == 1 then
+    return _search(pat, opts_match.engines[1], opts_leap)
+  end
+
+  -- if multiple engines, return merged matches
+  local data = {}
+  for _, engine in pairs(opts_match.engines) do
+    local ok, matches = pcall(_search, pat, engine, opts_leap)
+    if ok then
+      for _, m in pairs(matches) do
+        local row, col = m.pos[1], m.pos[2]
+        if not data[row] then
+          data[row] = {}
+        end
+        if not data[row][col] then
+          data[row][col] = m
+        end
+      end
+    end
+  end
+
+  local ret = {}
+  for _, poslist in pairs(data) do
+    for _, pos in pairs(poslist) do
+      table.insert(ret, pos)
+    end
+  end
+
+  return ret
+end
+
 ---@param pat string
 ---@param opts_match Opts_match?
 ---@param opts_leap table?
@@ -28,7 +70,7 @@ local function leap(pat, opts_match, opts_leap)
   local _opts_match = vim.tbl_deep_extend("force", opts_match or {}, opts_match_default)
   local _opts_leap = vim.tbl_deep_extend("keep", opts_leap or {}, { action = action })
   -- search for leap targets
-  _opts_leap.targets = _search(pat, _opts_match.engines[1], _opts_leap)
+  _opts_leap.targets = search(pat, _opts_match, _opts_leap)
   if #_opts_leap.targets == 0 then
     return false
   end
