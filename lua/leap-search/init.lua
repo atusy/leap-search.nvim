c8f497a3ce430c5ba48adda9ce39da2b0a5c6495
atusy <30277794+atusy@users.noreply.github.com>
2023-05-12 23:55:29 +0900

    refactor: find targets after LeapEnter

--- lua/leap-search/init.lua
+++ lua/leap-search/init.lua
@@ -21,51 +21,46 @@ local opts_match_default = {
 local function leap_main(pat, opts_match, opts_leap)
   local _opts_match = vim.tbl_deep_extend("keep", opts_match or {}, opts_match_default)
   local _opts_leap = vim.tbl_deep_extend("keep", opts_leap or {}, { action = require("leap-search.action").jump })
-  -- search for leap targets
-  _opts_leap.targets = require("leap-search.engine").search(pat, _opts_match, _opts_leap)
-  if #_opts_leap.targets == 0 then
-    return false
-  end
 
   -- prepare autocmd to be invoked on LeapEnter and removed subsequently
-  local autocmd
   local ns = vim.api.nvim_create_namespace("")
   local bufs = {}
   local function del()
-    pcall(vim.api.nvim_del_autocmd, autocmd)
     for b, _ in pairs(bufs) do
       vim.api.nvim_buf_clear_namespace(b, ns, 0, -1)
     end
   end
 
-  -- avoid hiding matched strings by placing labels before the matches
-  autocmd = vim.api.nvim_create_autocmd("User", {
-    pattern = "LeapEnter",
-    once = true,
-    callback = function()
-      if _opts_match.prefix_label ~= false then
-        for _, t in pairs(require("leap").state.args.targets) do
-          local b = (t.wininfo and t.wininfo.bufnr or 0)
-          bufs[b] = true
-          vim.api.nvim_buf_set_extmark(b, ns, t.pos[1] - 1, t.pos[2] - 1, {
-            end_col = t.pos[3] - 1,
-            hl_group = _opts_match.hl_group,
-          })
-          local p = t.pos[2]
-          if p > 1 then
-            t.pos[2] = p - 1
-            t.offset = 1
-          end
+  -- search for leap targets
+  local targets
+  _opts_leap.targets = function()
+    targets = require("leap-search.engine").search(pat, _opts_match, _opts_leap)
+    require("leap").state.args.targets = targets
+    if _opts_match.prefix_label ~= false then
+      for _, t in pairs(require("leap").state.args.targets) do
+        local b = (t.wininfo and t.wininfo.bufnr or 0)
+        bufs[b] = true
+        vim.api.nvim_buf_set_extmark(b, ns, t.pos[1] - 1, t.pos[2] - 1, {
+          end_col = t.pos[3] - 1,
+          hl_group = _opts_match.hl_group,
+        })
+        local p = t.pos[2]
+        if p > 1 then
+          t.pos[2] = p - 1
+          t.offset = 1
         end
       end
-      vim.api.nvim_create_autocmd("User", { pattern = "LeapLeave", callback = del })
+    end
 
-      local ok, w = pcall(require, "leap-wide")
-      if ok then
-        w.fix_labelling()
-      end
-    end,
-  })
+    vim.api.nvim_create_autocmd("User", { pattern = "LeapLeave", callback = del })
+
+    local ok, w = pcall(require, "leap-wide")
+    if ok then
+      w.fix_labelling()
+    end
+
+    return targets
+  end
 
   -- leap!
   local ok, err = pcall(require("leap").leap, _opts_leap)
@@ -75,7 +70,7 @@ local function leap_main(pat, opts_match, opts_leap)
     error(err)
   end
 
-  return true
+  return #targets > 0
 end
 
 ---@param pat? string
