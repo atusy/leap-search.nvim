42e2957ff06563b18102b69f031c4f929007bfb0
atusy <30277794+atusy@users.noreply.github.com>
2023-05-13 13:56:49 +0900

    fix: avoid E554 syntax error in \\{...} on parsing regex

--- lua/leap-search/engine/kensaku/query.lua
+++ lua/leap-search/engine/kensaku/query.lua
@@ -6,7 +6,11 @@
 ---@param opts_leap table
 local function search(pat, opts_engine, opts_leap)
   local query = vim.fn["kensaku#query"](pat)
-  return require("leap-search.engine.vim.regex").search(query, opts_engine, opts_leap)
+  local ok, res = pcall(require("leap-search.engine.vim.regex").search, query, opts_engine, opts_leap)
+  if not ok and string.find(tostring(res), "Vim:E554: Syntax error in \\{...}", nil, true) then
+    return require("leap-search.engine.vim.regex").search(query:gsub("\\{", "{"):gsub("\\}", "}"), opts_engine, opts_leap)
+  end
+  error(res)
 end
 
 return {
