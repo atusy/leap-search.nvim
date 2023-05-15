43de8e6604a6d94974fd21f6dd71264e3000a5ba
atusy <30277794+atusy@users.noreply.github.com>
2023-05-15 10:06:30 +0900

    fix: a regression kensaku#query not working after 42e2957

--- lua/leap-search/engine/kensaku/query.lua
+++ lua/leap-search/engine/kensaku/query.lua
@@ -7,8 +7,15 @@
 local function search(pat, opts_engine, opts_leap)
   local query = vim.fn["kensaku#query"](pat)
   local ok, res = pcall(require("leap-search.engine.vim.regex").search, query, opts_engine, opts_leap)
-  if not ok and string.find(tostring(res), "Vim:E554: Syntax error in \\{...}", nil, true) then
-    return require("leap-search.engine.vim.regex").search(query:gsub("\\{", "{"):gsub("\\}", "}"), opts_engine, opts_leap)
+  if ok then
+    return res
+  end
+  if string.find(tostring(res), "Vim:E554: Syntax error in \\{...}", nil, true) then
+    return require("leap-search.engine.vim.regex").search(
+      query:gsub("\\{", "{"):gsub("\\}", "}"),
+      opts_engine,
+      opts_leap
+    )
   end
   error(res)
 end
