ce9a674ddf498f9c2494593546a9fc5a0740a773
atusy <30277794+atusy@users.noreply.github.com>
2023-06-19 23:46:43 +0900

    fix: jump action should add current position to jumplist

--- lua/leap-search/action.lua
+++ lua/leap-search/action.lua
@@ -4,7 +4,7 @@ function M.jump(t)
   local r, c = t.pos[1], t.pos[2] + (t.offset or 0)
   require("leap.jump")["jump-to!"]({ r, c }, {
     winid = vim.api.nvim_get_current_win(),
-    ["add_to_jumplist?"] = true,
+    ["add-to-jumplist?"] = true,
     mode = "n",
     offset = require("leap").state.args.offset or 0,
     ["backward?"] = false,
